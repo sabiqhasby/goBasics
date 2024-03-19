1cdbcefbcbdad030baa551ab7e2b23f66c2f4588
Sabiq <sabiq@Tegsas-MacBook-Pro.local>
2024-03-20 06:24:30 +0700

    adding generic

--- DEMO/note/main.go
+++ DEMO/note/main.go
@@ -9,7 +9,25 @@ import (
 	"strings"
 )
 
+type saver interface {
+	Save() error
+}
+
+// type displayer interface {
+// 	Display()
+// }
+
+type outputable interface {
+	saver
+	Display()
+}
+
 func main() {
+
+	printSomething(1)
+	printSomething(1.5)
+	printSomething("Something")
+
 	title, content := getNoteData()
 	todoText := getUserInput("Todo text: ")
 
@@ -18,12 +36,8 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	todo.Display()
-	err = todo.Save()
-	if err != nil {
-		fmt.Println("Saving the todo failed")
-		return
-	}
+	outputData(todo)
+
 	fmt.Println("Saving todo succesfull")
 
 	userNote, err := note.New(title, content)
@@ -32,19 +46,57 @@ func main() {
 		return
 	}
 
-	userNote.Display()
-	err = userNote.Save()
-	if err != nil {
-		fmt.Println("Saving the note failed")
-		return
-	}
+	outputData(userNote)
 
 	fmt.Println("Saving the note succesfull")
 }
 
-func getTodoData() string {
-	text := getUserInput("Todo text: ")
-	return text
+func printSomething(value interface{}) {
+	//interface{} will accept any kind of value
+
+	intVal, ok := value.(int)
+
+	if ok {
+		fmt.Println("Integer:", intVal)
+		return
+	}
+
+	floatVal, ok := value.(float64)
+
+	if ok {
+		fmt.Println("Float:", floatVal)
+		return
+	}
+	stringVal, ok := value.(string)
+
+	if ok {
+		fmt.Println("String:", stringVal)
+		return
+	}
+
+	// switch value.(type) {
+	// case int:
+	// 	fmt.Println("Integer:", value)
+	// case float64:
+	// 	fmt.Println("Float:", value)
+	// case string:
+	// 	fmt.Println(value)
+	// }
+
+}
+
+func outputData(data outputable) error {
+	data.Display()
+	return saveData(data)
+}
+
+func saveData(data saver) error {
+	err := data.Save()
+	if err != nil {
+		fmt.Println("Saving the data failed")
+		return err
+	}
+	return nil
 }
 
 func getNoteData() (string, string) {
